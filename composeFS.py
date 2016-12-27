e472ea835fd9dfb68438ba60e274a26715610fd3
Mark E. Shoulson <mark@kli.org>
2016-12-27 14:18:01 -0500

    Fuse works with python3 fuse library. Amended: fixed raising errors, remove debugging etc.

--- composeFS.py
+++ composeFS.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from fuse import Fuse, Stat
 import fuse
 import stat
-fuse.fuse_python_api=(0,2)
 import sys
 import os
 import re
@@ -29,11 +27,11 @@ def DBGmsg(msg):
 
 # def debugfunc(func):
 #     def blah(*args, **kwargs):
-#         DBGmsg("Entering %s(%s :: %s)\n"%(func.func_name,
+#         DBGmsg("Entering %s(%s :: %s)\n"%(func.__name__,
 #                                                          repr(args),
 #                                                          repr(kwargs)))
 #         rv=func(*args, **kwargs)
-#         DBGmsg("Leaving %s (%s)\n"%(func.func_name, repr(rv)[:100]))
+#         DBGmsg("Leaving %s (%s)\n"%(func.__name__, repr(rv)[:100]))
 #         return rv
 #     return update_wrapper(blah,func)
 
@@ -47,7 +45,7 @@ def flattendict(dct, prefixes=None, rv=None):
         rv={}
     if prefixes is None:
         prefixes=[]
-    for (k, v) in dct.iteritems():
+    for (k, v) in dct.items():
         if isinstance(v, dict):
             flattendict(v, prefixes+[k], rv)
         else:
@@ -60,14 +58,13 @@ def flatascompose(dct, stream=sys.stdout):
     # it in order of lineno.
     try:
         #  XXX Gonna have to change for py3
-        allentries=sorted(dct.items(), cmp=lambda a,b : cmp(a[1][1],
-                                                            b[1][1]))
+        allentries=sorted(dct.items(), key=lambda x: x[1][1])
         for ent in allentries:
             try:
                 (key, data)=ent
                 (val, lineno, comments, inline)=data
-                stream.write(unicode(comments))
-                stream.write(u' '.join(u'<{0}>'.format(unicode(_)) for _ in key))
+                stream.write(str(comments))
+                stream.write(u' '.join(u'<{0}>'.format(str(_)) for _ in key))
                 stream.write(u'\t:\t"{}"'.format(val))
                 if inline:
                     if len(val)==1:
@@ -83,9 +80,9 @@ def flatascompose(dct, stream=sys.stdout):
                         stream.write(u'\tU{:04X}\t# {}\n'.format(ord(val), "????"))
                 else:
                     stream.write(u"\n")
-            except (Exception,Error) as E:
+            except (Exception,) as E:
                 pass
-    except (Exception,Error) as E:
+    except (Exception,) as E:
         DBGmsg(u"AAH! "+str(E)+u"\n")
     finally:
         DBGmsg(u"\nAnd done now.\n")
@@ -100,7 +97,7 @@ def readfile(*files):
         with closing(open(filename,"r")) as fd:
             for line in fd:
                 linecount+=1
-                line=line.decode('utf-8')
+                #line=line.decode('utf-8')
                 startpos=0
                 name=[]
                 dupsfound=[]
@@ -109,28 +106,28 @@ def readfile(*files):
                     if not m:
                         break
                     word=m.group(1)
-                    name.append(str(word)) # The keys are ordinary strings, not unicode
+                    name.append(str(word))
                     startpos+=m.end()
                 if startpos<=0:
                     comments+=line
                     continue
-                m=re.match(ur'[^"]*"(.+?)"',line)
+                m=re.match(r'[^"]*"(.+?)"',line)
                 if not m:
                     # shouldn't happen, but just in case
                     val=u'???'
                     print("couldn't make sense of line: "+line)
                 else:
-                    val=unicode(m.group(1))
+                    val=str(m.group(1))
                 # Can't otherwise distinguish between auto-inlines and custom...
-                m=re.search(ur'## *(.*)$', line)
+                m=re.search(r'## *(.*)$', line)
                 if m:
-                    inline=unicode(m.group(1))
+                    inline=str(m.group(1))
                 else:
                     inline=u""
                 cur=listing
                 for elt in name[:-1]:
                     if type(cur)==dict:
-                        if not cur.has_key(elt):
+                        if not elt in cur:
                             cur[elt]={}
                         cur=cur[elt]        # This will fail for prefix conflicts
                     else:
@@ -149,15 +146,13 @@ def readfile(*files):
         listing[" ENDING "]=(u"", lineno, comments)
     return listing
 
-class ComposeFuse(Fuse):
-    # DBG=open("/home/mark/xcompose/TREEDUMP","w")
+class ComposeFuse(fuse.Operations):
+    DBG=open("/home/mark/xcompose/TREEDUMP","w")
 
     fieldsep='-'
-    def __init__(self, *args, **kwargs):
-        Fuse.__init__(self, *args, **kwargs)
 
     @debugfunc
-    def fsinit(self):
+    def init(self, *args):
         infiles=self.infile.split('|')
         self.listing=readfile(*list(os.path.expanduser(_) for _ in infiles))
         print(repr(self.listing))
@@ -182,13 +177,10 @@ class ComposeFuse(Fuse):
             self.errors='strict'
 
     @debugfunc
-    def fsdestroy(self):
+    def destroy(self, *args, **kwargs):
         if self.outfile:
-            try:
-                with closing(io.open(self.outfile,"w",encoding='utf-8',newline=u"\n")) as fd:
-                    flatascompose(flattendict(self.listing), stream=fd)
-            except (Exception, Error) as E:
-                pass
+            with closing(io.open(self.outfile,"w",encoding='utf-8',newline=u"\n")) as fd:
+                flatascompose(flattendict(self.listing), stream=fd)
 
     @staticmethod
     def getParts(path):
@@ -230,8 +222,7 @@ class ComposeFuse(Fuse):
         return isinstance(rv, dict)
 
     @debugfunc
-    def getattr(self, path):
-        st=Stat()
+    def getattr(self, path, fh=None):
         suffix=None
         # The names are very limited, and this only happens for leaves.
         try:
@@ -242,35 +233,33 @@ class ComposeFuse(Fuse):
         pathelts=self.getParts(path)
         elt=self.followpath(pathelts)
         if elt is None:
-            return -fuse.ENOENT
+            raise fuse.FuseOSError(fuse.ENOENT)
         if self.is_directory(None, pathelts):
-            st.st_mode=stat.S_IFDIR | 0o755
-            st.st_nlink=2
-            st.st_ctime=0
-            st.st_atime=0
-            st.st_mtime=0
+            st=dict(st_mode=stat.S_IFDIR | 0o755,
+                    st_nlink=2,
+                    st_ctime=0,
+                    st_atime=0,
+                    st_mtime=0)
         else:
-            st.st_mode=stat.S_IFREG | 0o644
-            st.st_nlink=1
-            st.st_size=len(elt[0].encode(self.encoding))
-            st.st_ctime=elt[1]*3600*24
-            st.st_atime=st.st_ctime
-            st.st_mtime=st.st_ctime
+            st=dict(st_mode=stat.S_IFREG | 0o644,
+                    st_nlink=1,
+                    st_size=len(elt[0].encode(self.encoding)),
+                    st_ctime=elt[1]*3600*24,
+                    st_atime=elt[1]*3600*24,
+                    st_mtime=elt[1]*3600*24)
             if suffix=='COMMENTS':
-                st.st_size=len(elt[2].encode(self.encoding))
+                st['st_size']=len(elt[2].encode(self.encoding))
             elif suffix=='INLINE':
-                st.st_size=len(elt[3].encode(self.encoding))
+                st['st_size']=len(elt[3].encode(self.encoding))
             elif suffix:        # not one of the allowed ones.
-                return -fuse.ENOENT
-        st.st_ino=0
-        st.st_def=0
-        st.st_uid=0
-        st.st_gid=0
+                raise fuse.FuseOSError(fuse.ENOENT)
+        st['st_ino']=0
+        st['st_def']=0
+        st['st_uid']=0
+        st['st_gid']=0
         return st
 
-    @debugfunc
-    def readlink(self, path):
-        return -fuse.ENOENT
+    readlink=None
 
     def readdir(self, path, offset):
         pathelts=self.getParts(path)
@@ -279,21 +268,19 @@ class ComposeFuse(Fuse):
         else:
             elt=self.followpath(pathelts)
         if elt is None:
-            yield -fuse.ENOENT
-            return
+            raise fuse.FuseOSError(fuse.ENOENT)
         if not self.is_directory(None, pathelts):
-            yield -fuse.ENOTDIR
-            return
-        yield fuse.Direntry('.')
-        yield fuse.Direntry('..')
+            raise fuse.FuseOSError(fuse.ENOTDIR)
+        yield '.'
+        yield '..'
         entries=sorted(elt.keys())
         for en in entries:
             if not isinstance(elt[en], dict):
                 if elt[en][2]:  # Has comments.
-                    yield fuse.Direntry(en+self.fieldsep+"COMMENTS")
+                    yield en+self.fieldsep+"COMMENTS"
                 if elt[en][3]:  # Has inline
-                    yield fuse.Direntry(en+self.fieldsep+"INLINE")
-            yield fuse.Direntry(en)
+                    yield en+self.fieldsep+"INLINE"
+            yield en
         return
 
     @debugfunc
@@ -302,14 +289,14 @@ class ComposeFuse(Fuse):
             pathelts=self.getParts(path)
             parent=self.followpath(pathelts[:-1])
             if parent is None:
-                return -fuse.ENOENT
+                raise fuse.FuseOSError(fuse.ENOENT)
             if not isinstance(parent, dict):
-                return -fuse.EEXIST
+                raise fuse.FuseOSError(fuse.EEXIST)
             if pathelts[-1] in parent:
-                return -fuse.EEXIST
+                raise fuse.FuseOSError(fuse.EEXIST)
             parent[pathelts[-1]]=''
         else:
-            return -fuse.ENOSYS
+            raise fuse.FuseOSError(fuse.ENOSYS)
 
     @debugfunc
     def unlink(self, path):
@@ -330,14 +317,14 @@ class ComposeFuse(Fuse):
         pathelts=self.getParts(path)
         elt=self.followpath(pathelts[:-1])
         if elt is None or not isinstance(elt, dict):
-            return -fuse.ENOENT
+            raise fuse.FuseOSError(fuse.ENOENT)
         if pathelts[-1] in elt:
-            return -fuse.EEXIST
+            raise fuse.FuseOSError(fuse.EEXIST)
         elt[pathelts[-1]]={}
         return
 
     @debugfunc
-    def write(self, path, buf, offset):
+    def write(self, path, buf, offset, fh=None):
         suffix=None
         try:
             (realpath,suffix)=path.rsplit(self.fieldsep,1)
@@ -348,26 +335,26 @@ class ComposeFuse(Fuse):
         parent=self.followpath(pathelts[:-1])
         item=parent.get(pathelts[-1], '')
         if isinstance(item, dict):
-            return -fuse.EISDIR
+            raise fuse.FuseOSError(fuse.EISDIR)
         # The item is a tuple, can't replace it in pieces.
         newitem=list(item)
         # Val, lineno, comments, inline
         if suffix=="COMMENTS":
-            newitem[2]=unicode(buf) # offset?
+            newitem[2]=str(buf) # offset?
         elif suffix=="INLINE":
-            newitem[3]=unicode(buf)
+            newitem[3]=str(buf)
         elif suffix:
-            return -fuse.ENOENT
+            raise fuse.FuseOSError(fuse.ENOENT)
         else:
-            newitem[0]=unicode(buf)
+            newitem[0]=str(buf)
         parent[pathelts[-1]]=tuple(newitem)
-        return len(unicode(buf).encode(self.encoding))
+        return len(str(buf).encode(self.encoding))
 
     def truncate(self, path, length):
         return 0                # Whatever...
 
     @debugfunc
-    def read(self, path, size, offset):
+    def read(self, path, size, offset, fh=None):
         suffix=None
         try:
             (realpath,suffix)=path.rsplit(self.fieldsep,1)
@@ -376,7 +363,7 @@ class ComposeFuse(Fuse):
             pass
         elt=self.followpath(self.getParts(path))
         if isinstance(elt, dict):
-            return -fuse.EISDIR
+            raise fuse.FuseOSError(fuse.EISDIR)
         if suffix=='COMMENTS':
             return elt[2][offset:offset+size+1].encode(self.encoding)
         elif suffix=='INLINE':
@@ -392,28 +379,37 @@ class ComposeFuse(Fuse):
     def open(self, path, flags):
         return 0
 
-    @debugfunc
-    def symlink(self, *args):
-        return -fuse.ENOSYS
+    symlink=None
 
-    @debugfunc
-    def link(self, *args):
-        return -fuse.ENOSYS
+    link=None
 
     @debugfunc
     def chmod(self, *args):
         pass
 
-try:
-    server=ComposeFuse(version="%prog "+fuse.__version__,
-                       usage='', dash_s_do='setsingle')
+if __name__ == '__main__':
+    server=ComposeFuse()
     server.path=os.getcwd()
-    server.parser.add_option(mountopt="infile")
-    server.parser.add_option(mountopt="outfile")
-    server.parser.add_option(mountopt="encoding")
-    server.parser.add_option(mountopt="errors")
-    server.parse(errex=1, values=server)
-    server.fsinit()
-    server.main()
-except AttributeError:           # Catch python -i calls
-    pass
+
+    # simple parsing, not bothering with true option parser.
+    # infile, outfile, [encoding], [errors]
+    if sys.argv[1].startswith("-o"):
+        mntpt=sys.argv[2]
+        for opt in sys.argv[1][2:].split(","):
+            try:
+                nam, val = opt.split('=',2)
+            except ValueError:
+                nam, val = opt, True
+            if not val:
+                val=True        # opt=
+            # If you abuse this to overwrite stuff the class needs,
+            # you deserve whatever you get.
+            setattr(server, nam, val)
+    else:
+        mntpt=sys.argv[1]
+
+
+    if not hasattr(server, 'infile') or not server.infile:
+        print("Need infile.")
+        exit(1)
+    fu=fuse.FUSE(server, mntpt, foreground=False, nothreads=True)
